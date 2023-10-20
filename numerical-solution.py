7a5c832d98e2a94017ada7a4b52675b78f285d75
Logan Yamamoto <lkyamamo@usc.edu>
2023-10-19 17:45:38 -0700

    initial commit

---
+++ numerical-solution.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+D = 2
+N = 50
+k = 1 #coulomb's constant
+q = 1 #unit charge
+
+#all calculations of potential assuming 0 at infinity
+
+def spinningDipolePotential(location):
+    return k*q/distance
+
+def calculatePotential2D(func, data, origin):
+    for i in range(N):
+        for j in range(N):
+
+
+def finiteDifferenceScheme():
+
+
+class dipoleSystem:
+    def __init__(self, location1, location2, angularVelocity):
+        
